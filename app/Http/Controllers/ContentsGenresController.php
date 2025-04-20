a8ddd4e9fb2bf98b46e4cb729627c6239666a22f
XR-Dante <telegram77012@gmail.com>
2025-04-20 19:16:41 +0500

    PATCH: Added migration

---
+++ app/Http/Controllers/ContentsGenresController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreContents_genresRequest;
+use App\Http\Requests\UpdateContents_genresRequest;
+use App\Models\Contents_genres;
+
+class ContentsGenresController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreContents_genresRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Contents_genres $contents_genres)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Contents_genres $contents_genres)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateContents_genresRequest $request, Contents_genres $contents_genres)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Contents_genres $contents_genres)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $users = \App\Models\User::all();
     return view('welcome', ['users' => $users]);
 });
+
+Route::resource('authors', AuthorController::class);
+
+//Route::get('/authors', function () {
+//    $authors = \App\Models\Author::all();
+//    return view('welcome', ['authors' => $authors]);
+//});
