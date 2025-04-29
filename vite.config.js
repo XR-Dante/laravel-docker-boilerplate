0e4dea097abf1d3f1c9f47951badd6c26472d066
XR-Dante <telegram77012@gmail.com>
2025-04-29 20:41:59 +0500

    MINOR: Installed breeze

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,16 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0', // Docker konteynerdan tashqariga ochish uchun
+        port: 5173,      // Vite frontend porti
+        hmr: {
+            host: 'localhost', // Localhost orqali hot reload qilish
+        },
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/js/app.js'], // Laravel Vite kirish fayli
             refresh: true,
         }),
         vue({
